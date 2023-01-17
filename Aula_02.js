cea295a9464f2f04bb1f5caf40da29f75b61fdcc
danielmassita <daniel.massita@gmail.com>
2023-01-17 15:04:07 -0300

    Fim da Aula 02 - História do JS

--- Aula_02.js
+++ Aula_02.js
@@ -3,6 +3,59 @@
 
 /*
 Você sabe qual foi a empresa a criar o JavaScript? Sabe qual é a diferença entre as linguagens Java e JavaScript? Sabe qual é a relação que existe entre as linguagens JavaScript e ECMAScript? Sabia que, por exemplo, o programa usado para acessar WhatsApp no computador é feito em JavaScript?
+
+PERGUNTAS (da aula anterior)
+- Diferença entre client e server?
+- Na estrutura de redes, qual a tarefa de cada uma das partes (client-server-side)?
+- Quais as diferenças entre HTML, CSS e JS?
+- Quatro sites que usam JS?
+
+NESSA AULA
+- JavaScript e Java são a mesma coisa?
+- De onde a tecnologia veio e pra onde vai?
+- JavaScript e ECMAscript, qual a relação?
+
+LINHA DO TEMPO 
+- 1960-1970... (primórdios da internet, guerra fria, informações, pscicológica e tecnológica)
+- URSS lança o Sputinik
+- 1960's Eisenhower decretou a criação da DARPA (tecnologia pra guerra)
+- Centro Militar tinha UM computador cheio de dados (se a bomba caisse, os dados seriam perdidos)
+- A idéia da DARPA era criar rede para os dados, em caso de dano físico, criou-se ARPANET
+- ARPANET = Univ. Califórnia, MIT, Militares, novas pequenas redes...
+- INTERNET
+
+- ARPANET -> INTERNET
+- 1993: Tim Berners-Lee criou algo pra pegar o conteúdo e dinamizar (HTML), (HTTP), (WWW), CERN (Comis. Europ. Research Nuclear)...
+- Navegador MOSAIC (criação da NCSA pelo Mark Anderson) 
+- 1994: Mark Anderson saiu pra trabalhar na NETSCAPE (com um milionário investidor)
+- 1994: NETSCAPE virou o primeiro navegador comercial (uma equipe da empresa se chamava Godzilla)
+- 1995: HTML era muito estático, alguém veio da Sylicon Graphics (Branden Eike) pra NETSCAPE, usou a base de conhecimento e fez o MOCHA...
+- 1995: MOCHA foi criada, mas na mídia existia a linguagem "Java" (da SunSystem) do futuro, então o Mocha (depois LiveScript) virou o JavaScript (jogo de marketing)...
+
+- SURGE O JAVASCRIPT
+- A Microsoft copiou a linguagem do JS e usou o código básico do Mosaic (NCSA, comprada pela Microsoft) e criou o Internet Explorer
+- 1995: Criação do Windows 95 + Internet Explorer usou a cópia e enfiou a venda casada...
+- 1997: A NetScape pra fugir da manobra da Microsoft, foi até a ECMA (empresa de padronização, ISO) e usou o próprio código cedido do JavaScript e fez a ECMAscript...
+- ECMAscript é a linguagem padronizada (ISO)...
+
+- 2002: Encerramento da Netscape, IE foi embutido no S.O., deixando a Netscape pra trás...
+- 2002: Galera saiu da Netscape e foi pra Fundação Mozilla (galera da equipe Godzilla), e fez o Firefox... 
+- 2008: Google entrou na briga (Netscape, Microsoft, Google) com o Google Chrome...
+- 2008: Google Chrome trouxe o motor interno de JavaScript da versão V-8 (motor do Chrome que roda JS)., gerando códito JIT (just in time) e código aberto (livre, logo um grupo fez um motor V-8 fora do Google Chrome, chamado Node.JS)...
+- 2010: Node.JS roda o JS fora do navegador, podendo rodar no server-side (do front pro back e BD)...
+
+ECMASCRIPT
+- Versão padronizada do JS (1997, versão 1.0)...
+- V. 2.0 (1998), v. 3.0 (1999) e os projetos ficacram travados (v. 4.0 nem existiu)...
+- ECMAscript se chamou ES5 pra englobar Json e Arrays
+- ES6 (let, const, strings e template) e mudou de nome
+- ECMA2016 (operador exponenciação), ECMA2017 (funções assíncronas e etc.), ECMA2018 (regex e promisses), etc...
+
+Angular, AngularJS, React (facebook), Vue (usam o JS em outro patamar), mas é necessário dominar o JavaScript para poder depois entender outras coisas J-Query (biblioteca), etc... Electron (github com interface gráfica), VS Code, Whatsapp Web, etc...
+
+TODAS ESSAS TECNOLOGIAS só podem ser abertas após um domínio sério do JavaScript, para não nos tornarmos um mero "copiador de código". 
+
+FrameWork, Biblioteca, SDK?
 */
 
 
