9bb94dce18aa878055b6fa4c5c17da6fd1c6a61c
danielmassita <daniel.massita@gmail.com>
2023-01-18 13:51:04 -0300

    Iniciando Aula 04

---
+++ Aula_04.js
@@ -0,0 +1,10 @@
+// Aula 04 - Criando o seu primeiro script
+// https://youtu.be/OmmJBfcMJA8
+
+/*
+Você já sabe diferenciar dentro do seu código, os trechos em HTML5, em CSS3 e em JavaScript? Sabe organizar as pastas do seu projeto dentro do Visual Studio Code? Sabe como testar se o Node.js está devidamente instalado? Já sabe utilizar os comandos alert, confirm e prompt do JavaScript? 
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.
+*/
