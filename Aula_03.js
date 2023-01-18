5351da6d507b363aa7f3720447900013a5574866
danielmassita <daniel.massita@gmail.com>
2023-01-18 13:27:50 -0300

    Aula 03 estudando...

---
+++ Aula_03.js
@@ -0,0 +1,27 @@
+// Aula 03 - Dando os Primeiros Passos
+// https://youtu.be/FdePtO5JSd0
+/* 
+Quais são os melhores livros de JavaScript em Português? Onde ter acesso à documentação oficial do JavaScript em Português e Inglês? Quais são os requisitos de software para aprender a programar em JavaScript? Qual é o melhor editor para códigos JavaScript?  Como instalar o Node.js no seu computador? Como configurar o Node.js? Para aprender JavaScript, é realmente necessário saber muito Inglês?Você está precisando de dicas para estudar e aprender de verdade?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.
+*/
+
+/*
+PERGUNTAS AULA ANTERIOR 
+- Qual empresa crisou a linguagem JavaScript? Netscape tornou famosa.
+- JavaScript e Java são a mesma coisa? Não, o Mocha/Livescript apenas surfou a onda do marketing do termo "java" como sendo a linguagem do futuro, e lançou o JavaScript.
+- ECMAScript tem relação com Javascript? Sim, o ECMA foi a versão padronizada européia (tipo ISO) que recebeu a base do Javascript pra fazer frente à concorrência da Microsoft. A Microsoft com seu navegador (Internet Explorer copiando o código base do Mosaic na incorporação da NCSA pela Microsoft) integrado no S.O. e PC's (domésticos).
+*/
+
+/*
+Exemplo de Aprendizagem
+- Aprender a tocar violão (apenas com vídeo, funciona)? Na festa, violão na mão, você toca alguma coisa?
+- Aprender a fazer manobras de bicicleta (apenas com vídeo, funciona)? Sair agora e pegar a bike, você faz todas as manobras?
+
+Então, com Programação precisa COLOCAR A MÃO NA MASSA .:. Prática é a chave...
+
+ 
+
+*/
